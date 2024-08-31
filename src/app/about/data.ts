1433acdc8bd9510fd95acac5a5bd45330ee7c026
Carlltz <56845820+Carlltz@users.noreply.github.com>
2024-08-31 23:39:57 +0200

    Update profile page and add new internfunkisar (#3)
    * Better about page and prepare for data
    
    * Add internfunkisar
    
    * Add descriptions and design

---
+++ src/app/about/data.ts
@@ -0,0 +1,143 @@
+import { Person, Role } from './types';
+
+export const internFunkisar: Record<Role, Person[]> = {
+    'konglig-lokalchef-data': [
+        {
+            name: 'Carl Blomqvist',
+            email: 'lokalchef',
+            emailType: 'data',
+        },
+    ],
+    'sektionslokalsansvarig-media': [],
+    stadskrii: [],
+    flippermicke: [
+        {
+            name: 'Samuel Hörnell',
+            email: 'shornell',
+            emailType: 'data',
+        },
+    ],
+    arkadmicke: [],
+    laskkylsansvarig: [
+        {
+            name: 'Carl Liljencrantz',
+            email: 'liljencr',
+            emailType: 'data',
+        },
+    ],
+    dlol: [
+        {
+            name: 'Karl-Isac Åström',
+            email: 'd-lol',
+            emailType: 'data',
+        },
+    ],
+    maestro: [
+        {
+            name: 'Casimir Bähr',
+            email: 'casimirb',
+            emailType: 'data',
+        },
+    ],
+    mario: [
+        {
+            name: 'Samuel Hörnell',
+            email: 'shornell',
+            emailType: 'data',
+        },
+        {
+            name: 'Lukas Malmberg',
+            email: 'lukmal',
+            emailType: 'data',
+        },
+        {
+            name: 'Sofie Bälter',
+            email: 'sbalter',
+            emailType: 'data',
+        },
+    ],
+    mister: [
+        {
+            name: 'Soma Szabo',
+            email: 'somasz',
+            emailType: 'data',
+        },
+    ],
+    misty: [],
+    mog: [
+        {
+            name: 'Olof Bargholtz Melcherson',
+            email: 'olofbm',
+            emailType: 'data',
+        },
+    ],
+    moms: [],
+    money: [
+        {
+            name: 'Jonathan Blomlöf',
+            email: 'jblomlof',
+            emailType: 'data',
+        },
+        {
+            name: 'Lukas Malmberg',
+            email: 'lukmal',
+            emailType: 'data',
+        },
+    ],
+    mopp: [
+        {
+            name: 'Casimir Bähr',
+            email: 'casimirb',
+            emailType: 'data',
+        },
+        {
+            name: 'Samuel Hörnell',
+            email: 'shornell',
+            emailType: 'data',
+        },
+    ],
+    movie: [],
+    moist: [
+        {
+            name: 'Emma Ericsson',
+            email: 'sieric',
+            emailType: 'data',
+        },
+        {
+            name: 'Soma Szabo',
+            email: 'somasz',
+            emailType: 'data',
+        },
+    ],
+    mucka: [
+        {
+            name: 'Melvin Jakobsson',
+            email: 'melvinj',
+            emailType: 'data',
+        },
+        {
+            name: 'Adam Sjöberg',
+            email: 'adamsjo',
+            emailType: 'data',
+        },
+    ],
+    mums: [
+        {
+            name: 'Axel Bergengren',
+            email: 'abergeng@kth.se',
+            emailType: 'custom',
+        },
+        {
+            name: 'Sofie Bälter',
+            email: 'sbalter',
+            emailType: 'data',
+        },
+    ],
+    matrix: [
+        {
+            name: 'Carl Liljencrantz',
+            email: 'liljencr',
+            emailType: 'data',
+        },
+    ],
+};
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,127 +2,53 @@ import React from 'react';
 import Info from '@/components/Info';
 import Profile from '@/components/Profile';
 import styles from './page.module.css';
+import { useMETAdorer } from './helpers';
+import { sortedRoles } from './types';
 
 export default function About() {
+    const internFunkisar = useMETAdorer();
+
     return (
         <div>
             <Info title="Vilka är METAdorerna?">
-                <p>Här hittar du information och kontaktuppgifter till internfunktionärsposterna.</p>
-                <p>
-                    Vill du gå med? Fyll i formuläret här: <a href="/#">länk</a>
-                </p>
+                <div className={styles.infoTextContainer}>
+                    <p style={{ lineHeight: '1.1em', textAlign: 'center' }}>
+                        METAdorerna är en delad nämnd mellan datasektionen och sektionen för medieteknik. Vi är de som
+                        tar hand om vår fantastiska sektionslokal META och ser till att den är en trevlig plats att
+                        vistas på. Vi har möten varje fredaglunch som just du gärna får komma på! För att få info om var
+                        mötet hålls så går du lättast med i METAdorerna genom formuläret nedan.
+                    </p>
+                    <h3>
+                        Vill du gå med? Fyll i formuläret:{' '}
+                        <a
+                            href="https://www.dsekt.se/blimetad"
+                            target="_blank">
+                            Bli METAd
+                        </a>
+                    </h3>
+                </div>
             </Info>
+            <div className={[styles.roles, styles.lokalchefer].join(' ')}>
+                {sortedRoles.slice(0, 2).map((role) => {
+                    return (
+                        <Profile
+                            key={role}
+                            role={role}
+                            people={internFunkisar[role].length ? internFunkisar[role] : [{ name: 'Vakant' }]}
+                        />
+                    );
+                })}
+            </div>
             <div className={styles.roles}>
-                <Profile
-                    role="Konglig Lokalchef (Data)"
-                    name="Carl Liljencrantz"
-                    email="lokalchef"
-                />
-                <Profile
-                    role="Sektionslokalsansvarig (Media)"
-                    name="Förnamn Efternamn"
-                    email="sektionslokal"
-                    media={true}
-                />
-                <Profile
-                    role="Städskrii (Data)"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-                <Profile
-                    role="Städskrii (Media)"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-                <Profile
-                    role="Läskkylsansvarig"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="dLol"
-                    name="Förnamn Efternamn"
-                    email="dlol"
-                />
-
-                <Profile
-                    role="Maestro"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Mario"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Mister"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Misty"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Moba"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Moms"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Money"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Mopp"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Movie"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Moist"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Mucka"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Mums"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
-
-                <Profile
-                    role="Matrix"
-                    name="Förnamn Efternamn"
-                    email=""
-                />
+                {sortedRoles.slice(2).map((role) => {
+                    return (
+                        <Profile
+                            key={role}
+                            role={role}
+                            people={internFunkisar[role].length ? internFunkisar[role] : [{ name: 'Vakant' }]}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
