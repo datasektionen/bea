bcf81546c681061c40d1b250837ac274af0b4e1b
Oskar Engström <oskar.fv57@gmail.com>
2025-11-11 12:56:23 +0100

    chore: update funkis names

--- src/app/(main)/about/data.ts
+++ src/app/(main)/about/data.ts
@@ -1,237 +1,282 @@
-import { Person, Role } from './types';
+import { Person, Role } from "./types";
 
 export const internFunkisar: Record<Role, Person[]> = {
-    'konglig-lokalchef-data': [
-        {
-            name: 'Carl Blomqvist',
-            email: 'lokalchef',
-            emailType: 'data',
-        },
-    ],
-    'sektionslokalsansvarig-media': [
-        {
-            name: 'Andreas Wingqvist',
-            email: 'sektionslokal',
-            emailType: 'media',
-        },
-    ],
-    stadskrii: [
-        {
-            name: 'Hannes Hultin',
-            email: 'hanneshu',
-            emailType: 'data',
-        },
-    ],
-    arkadmicke: [
-        {
-            name: 'Dmitry Chirin',
-            email: 'dmitryc',
-            emailType: 'data',
-        },
-        {
-            name: 'Oscar Eriksson',
-            email: 'oscae@kth.se',
-            emailType: 'custom',
-        },
-        {
-            name: 'Natan Söderström',
-            email: 'natans@kth.se',
-            emailType: 'custom',
-        },
-    ],
-    flippermicke: [
-        {
-            name: 'Samuel Hörnell',
-            email: 'shornell',
-            emailType: 'data',
-        },
-        {
-            name: 'Elias Engström',
-            email: 'elengst@kth.se',
-            emailType: 'custom',
-        },
-    ],
-    laskkylsansvarig: [
-        {
-            name: 'Carl Liljencrantz',
-            email: 'liljencr',
-            emailType: 'data',
-        },
-        {
-            name: 'Fredrik Blomqvist',
-            email: 'frblo',
-            emailType: 'data',
-        },
-    ],
-    dlol: [
-        {
-            name: 'Karl-Isac Åström',
-            email: 'd-lol',
-            emailType: 'data',
-        },
-    ],
-    maestro: [
-        {
-            name: 'Casimir Bähr',
-            email: 'casimirb',
-            emailType: 'data',
-        },
-        {
-            name: 'Olof Bargholtz Melcherson',
-            email: 'olofbm',
-            emailType: 'data',
-        },
-    ],
-    mario: [
-        {
-            name: 'Samuel Hörnell',
-            email: 'shornell',
-            emailType: 'data',
-        },
-        {
-            name: 'Lukas Malmberg',
-            email: 'lukmal',
-            emailType: 'data',
-        },
-    ],
-    mister: [
-        {
-            name: 'Karmela Eliasson',
-            email: 'karmela@kth.se',
-            emailType: 'custom',
-        },
-    ],
-    misty: [
-        {
-            name: 'Karl-Isac Åström (Kaffe)',
-            email: 'kiastrom',
-            emailType: 'data',
-        },
-        {
-            name: 'Kristin Rosen (Te)',
-            email: 'kristinr@kth.se',
-            emailType: 'custom',
-        },
-    ],
-    moms: [
-        {
-            name: 'Linn Benali',
-            email: 'lbenali',
-            emailType: 'data',
-        },
-        {
-            name: 'Daniel Norman',
-            email: 'dnorma',
-            emailType: 'data',
-        },
-    ],
-    money: [
-        {
-            name: 'Jonathan Blomlöf',
-            email: 'jblomlof',
-            emailType: 'data',
-        },
-        {
-            name: 'Lucas Dahlgren',
-            email: 'elbda',
-            emailType: 'data',
-        },
-        {
-            name: 'Liam Thorell',
-            email: 'liamt@kth.se',
-            emailType: 'custom',
-        },
-    ],
-    mopp: [
-        {
-            name: 'Casimir Bähr',
-            email: 'casimirb',
-            emailType: 'data',
-        },
-        {
-            name: 'Samuel Hörnell',
-            email: 'shornell',
-            emailType: 'data',
-        },
-        {
-            name: 'Tore Forslin',
-            email: 'tforslin',
-            emailType: 'data',
-        },
-    ],
-    movie: [
-        {
-            name: 'Jennifer Köhler',
-            email: 'jenkoh',
-            emailType: 'data',
-        },
-        {
-            name: 'Nicklas Peng',
-            email: 'npeng',
-            emailType: 'data',
-        },
-        {
-            name: 'Klara Romlin',
-            email: 'kromlin',
-            emailType: 'data',
-        },
-    ],
-    moist: [
-        {
-            name: 'Emma Ericsson',
-            email: 'sieric',
-            emailType: 'data',
-        },
-        {
-            name: 'Soma Szabo',
-            email: 'somasz',
-            emailType: 'data',
-        },
-    ],
-    mucka: [
-        {
-            name: 'Melvin Jakobsson (Hall)',
-            email: 'melvinj',
-            emailType: 'data',
-        },
-        {
-            name: 'Adam Sjöberg (Pant)',
-            email: 'adamsjo',
-            emailType: 'data',
-        },
-        {
-            name: 'Filip Ramslöv (Catwalk)',
-            email: 'framslov',
-            emailType: 'data',
-        },
-    ],
-    mums: [
-        {
-            name: 'Axel Bergengren',
-            email: 'abergeng@kth.se',
-            emailType: 'custom',
-        },
-        {
-            name: 'Sofie Bälter',
-            email: 'sbalter',
-            emailType: 'data',
-        },
-        {
-            name: 'Isak Lefèvre',
-            email: 'lefevre',
-            emailType: 'data',
-        },
-    ],
-    matrix: [
-        {
-            name: 'Carl Liljencrantz',
-            email: 'liljencr',
-            emailType: 'data',
-        },
-        {
-            name: 'Alvin Westling',
-            email: 'alvinw@kth.se',
-            emailType: 'custom',
-        },
-    ],
+  "konglig-lokalchef-data": [
+    {
+      name: "Hannes Hultin",
+      email: "lokalchef",
+      emailType: "data",
+    },
+  ],
+  "sektionslokalsansvarig-media": [
+    {
+      name: "Vakant",
+      email: "sektionslokal",
+      emailType: "media",
+    },
+  ],
+  stadskrii: [
+    {
+      name: "Eynar Eklöf",
+      email: "eaeklof",
+      emailType: "data",
+    },
+  ],
+  arkadmicke: [
+    {
+      name: "Oscar Eriksson",
+      email: "oscae",
+      emailType: "data",
+    },
+  ],
+  flippermicke: [
+    {
+      name: "Emma Ericsson",
+      email: "sieric",
+      emailType: "data",
+    },
+    {
+      name: "Karl-Isac Åström",
+      email: "kiastrom",
+      emailType: "data",
+    },
+  ],
+  laskkylsansvarig: [
+    {
+      name: "Carl Blomqvist",
+      email: "cablomqv",
+      emailType: "data",
+    },
+    {
+      name: "Karl-Isac Åström",
+      email: "kiastrom",
+      emailType: "data",
+    },
+    {
+      name: "Lukas Malmberg",
+      email: "lukmal",
+      emailType: "data",
+    },
+    {
+      name: "Sofie Bälter",
+      email: "sbalter",
+      emailType: "data",
+    },
+  ],
+  dlol: [
+    {
+      name: "Abdelrahman (Abood) Aldaker",
+      email: "lol",
+      emailType: "data",
+    },
+  ],
+  maestro: [
+    {
+      name: "Dimitry Chirin",
+      email: "dmitryc",
+      emailType: "data",
+    },
+    {
+      name: "Josephine Jerselius",
+      email: "josjer@kth.se",
+      emailType: "custom",
+    },
+    {
+      name: "Karmela Eliasson",
+      email: "karmela",
+      emailType: "data",
+    },
+    {
+      name: "Samuel Hörnell",
+      email: "shornell",
+      emailType: "data",
+    },
+  ],
+  mario: [
+    {
+      name: "Carl Liljencrantz",
+      email: "liljencr",
+      emailType: "data",
+    },
+    {
+      name: "David Peilitz",
+      email: "dpeilitz",
+      emailType: "data",
+    },
+  ],
+  mister: [
+    {
+      name: "Alvin Yang",
+      email: "alviny@kth.se",
+      emailType: "custom",
+    },
+    {
+      name: "Simon Aksberg",
+      email: "saksberg@kth.se",
+      emailType: "custom",
+    },
+    {
+      name: "Tommy Bergman",
+      email: "tombergm@kth.se",
+      emailType: "custom",
+    },
+  ],
+  misty: [
+    {
+      name: "Hjalmar Fjällborg (Kaffe)",
+      email: "hfja",
+      emailType: "data",
+    },
+    {
+      name: "Vendela Bexander (Te)",
+      email: "vbex",
+      emailType: "data",
+    },
+    {
+      name: "Sofia Wallgren (Kaffe)",
+      email: "sofwall",
+      emailType: "data",
+    },
+    {
+      name: "Teo Vesterlund Gallet (Kaffe)",
+      email: "teovg@kth.se",
+      emailType: "custom",
+    },
+  ],
+  mkdir: [
+    {
+      name: "Carl Blomqvist",
+      email: "cablomqv",
+      emailType: "data",
+    },
+    {
+      name: "Johanna Plant",
+      email: "jplant",
+      emailType: "data",
+    },
+  ],
+  moms: [
+    {
+      name: "Hugo Karlsson",
+      email: "hugokar",
+      emailType: "data",
+    },
+    {
+      name: "Leonard Smedberg",
+      email: "lsme",
+      emailType: "data",
+    },
+    {
+      name: "Marcus Sörberg",
+      email: "msorberg@kth.se",
+      emailType: "custom",
+    },
+  ],
+  money: [
+    {
+      name: "Soma Szabo",
+      email: "somasz",
+      emailType: "data",
+    },
+    {
+      name: "Roger Chen",
+      email: "rogerche",
+      emailType: "data",
+    },
+    {
+      name: "Liam Thorell",
+      email: "liamt",
+      emailType: "data",
+    },
+  ],
+  mopp: [
+    {
+      name: "Amanda Edblad",
+      email: "amaed",
+      emailType: "data",
+    },
+    {
+      name: "Jonathan Blomlöf",
+      email: "jblomlof",
+      emailType: "data",
+    },
+  ],
+  movie: [
+    {
+      name: "Filip Ramslöv",
+      email: "framslov",
+      emailType: "data",
+    },
+    {
+      name: "Vendela Bexander",
+      email: "vbex",
+      emailType: "data",
+    },
+    {
+      name: "Roger Chen",
+      email: "rogerche",
+      emailType: "data",
+    },
+    {
+      name: "Agnes Lindencrona",
+      email: "lindencr",
+      emailType: "data",
+    },
+    {
+      name: "Ivar Boqvist",
+      email: "ivarbo",
+      emailType: "data",
+    },
+    {
+      name: "Nicklas Peng",
+      email: "npeng",
+      emailType: "data",
+    },
+    {
+      name: "Linn Benali",
+      email: "lbenali",
+      emailType: "data",
+    },
+  ],
+  moist: [
+    {
+      name: "Olof Bargholtz Melcherson",
+      email: "olofbm",
+      emailType: "data",
+    },
+    {
+      name: "Viktor Ekby",
+      email: "viktoe",
+      emailType: "data",
+    },
+  ],
+  mucka: [
+    {
+      name: "Fredrik Blomqvist (Catwalk)",
+      email: "frblo",
+      emailType: "data",
+    },
+    {
+      name: "Jennifer Köhler (Hall)",
+      email: "jenkoh",
+      emailType: "data",
+    },
+  ],
+  matrix: [
+    {
+      name: "Casimir Bähr",
+      email: "casimirb",
+      emailType: "data",
+    },
+    {
+      name: "Oskar Engström",
+      email: "osen",
+      emailType: "data",
+    },
+    {
+      name: "Daniel Hu",
+      email: "zhongmin@kth.se",
+      emailType: "custom",
+    },
+  ],
 };
